9dba4e486d630bc240c8db9be206cf09c25e9a7a
Riku <alamriku069@gmail.com>
2021-01-23 00:36:37 +0600

    book author route and controller added, json exeption type added

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,16 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+//Author
+$router->get('/authors', 'AuthorController@index');
+$router->post('/authors', 'AuthorController@store');
+$router->get('/authors/{author}', 'AuthorController@show');
+$router->put('/authors/{author}', 'AuthorController@update');
+$router->delete('/authors/{author}', 'AuthorController@destroy');
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+//Book
+$router->get('/books', 'BookController@index');
+$router->post('/books', 'BookController@store');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
