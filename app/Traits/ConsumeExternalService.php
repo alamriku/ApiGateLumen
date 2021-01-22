005a064f53c9363e6c8932aa0f13813a8696004a
Riku <alamriku069@gmail.com>
2021-01-23 00:51:48 +0600

    guzzle request setup on cosumerExterServcieTrait

---
+++ app/Traits/ConsumeExternalService.php
@@ -0,0 +1,18 @@
+<?php
+use GuzzleHttp\Client;
+trait ConsumeExternalService
+{
+    //send a request to any service
+    public function performRequest($method, $requestUrl, $formParams = [], $headers = [])
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        $response = $client->request($method, $requestUrl, ['form_params' => $formParams,
+            'headers' => $headers
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+}
