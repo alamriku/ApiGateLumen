2830cea9d80f58d9c43b12002f25ae1b9b0e3281
Riku <alamriku069@gmail.com>
2021-01-24 00:03:55 +0600

    bookService Crud done

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 
 use App\Services\BookService;
 use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class BookController extends Controller
 {
@@ -18,5 +20,28 @@ class BookController extends Controller
     public function __construct(BookService $bookService)
     {
         $this->bookService = $bookService;
+    }
+    public function index()
+    {
+        return $this->successResponse($this->bookService->obtainBooks());
+    }
+
+    public function store(Request $request)
+    {
+        return $this->successResponse($this->bookService->createBook($request->all()),Response::HTTP_CREATED);
+    }
+    public function show($book)
+    {
+        return $this->successResponse($this->bookService->obtainBook($book));
+    }
+
+    public function update(Request $request, $book)
+    {
+        return $this->successResponse($this->bookService->editBook($request->all(), $book));
+    }
+
+    public function destroy($book)
+    {
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
 }
--- app/services/BookService.php
+++ app/services/BookService.php
@@ -8,11 +8,36 @@ class BookService
 {
     use ConsumeExternalService;
 
-    //The base uri to consume the authors services
+    //The base uri to consume the books services
     public $baseUri;
 
     public function __construct()
     {
         $this->baseUri = config('services.books.base_uri');
+    }
+
+    // obtain full list of books from the other service
+    public function obtainBooks()
+    {
+        return $this->performRequest('GET', '/books');
+    }
+    // create a single author from the other service
+    public function createBook($data)
+    {
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    public function obtainBook($author)
+    {
+        return $this->performRequest('GET', "/books/{$author}");
+    }
+
+    public function editBook($data, $author)
+    {
+        return $this->performRequest('PUT', "/books/{$author}", $data);
+    }
+    public function deleteBook($author)
+    {
+        return $this->performRequest('DELETE', "/books/{$author}");
     }
 }
