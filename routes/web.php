b11d2d1d47612fdc60ed9e5fb2ccc324c31f6878
Riku <alamriku069@gmail.com>
2021-01-24 12:27:26 +0600

    middleware acitvate on app.php and added on route file web.php

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,19 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-//Author
-$router->get('/authors', 'AuthorController@index');
-$router->post('/authors', 'AuthorController@store');
-$router->get('/authors/{author}', 'AuthorController@show');
-$router->put('/authors/{author}', 'AuthorController@update');
-$router->delete('/authors/{author}', 'AuthorController@destroy');
+
+$router->group(['middleware' => 'client.credentials'], function () use ($router){
+    //Author
+    $router->get('/authors', 'AuthorController@index');
+    $router->post('/authors', 'AuthorController@store');
+    $router->get('/authors/{author}', 'AuthorController@show');
+    $router->put('/authors/{author}', 'AuthorController@update');
+    $router->delete('/authors/{author}', 'AuthorController@destroy');
 
 //Book
-$router->get('/books', 'BookController@index');
-$router->post('/books', 'BookController@store');
-$router->get('/books/{book}', 'BookController@show');
-$router->put('/books/{book}', 'BookController@update');
-$router->delete('/books/{book}', 'BookController@destroy');
+    $router->get('/books', 'BookController@index');
+    $router->post('/books', 'BookController@store');
+    $router->get('/books/{book}', 'BookController@show');
+    $router->put('/books/{book}', 'BookController@update');
+    $router->delete('/books/{book}', 'BookController@destroy');
+});
